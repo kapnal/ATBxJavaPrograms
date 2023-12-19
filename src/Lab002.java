5c39fa58158dcbb8858d22e5e264f107248d568b
kapnal <kapilnalwar1991@gmail.com>
2023-12-19 23:42:16 +0530

    First commit

---
+++ src/Lab002.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        byte age=10;   //Range of byte -128 to 127
+        age=127;
+        System.out.println(age);
+    }
+}
---
+++ src/Lab003.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+      byte age=127;
+        System.out.println(age+1);
+
+
+    }
+}
