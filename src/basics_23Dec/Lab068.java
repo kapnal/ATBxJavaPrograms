5f3200861952a208cd8a270a0632f1b8e4d73a32
kapnal <kapilnalwar1991@gmail.com>
2023-12-27 00:15:26 +0530

    23rd Dec

---
+++ src/basics_23Dec/Lab068.java
@@ -0,0 +1,30 @@
+package src.basics_23Dec;
+
+import java.util.Scanner;
+
+public class Lab068 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the X");
+        double x = sc.nextDouble();
+        System.out.println("Enter the Y");
+        double y = sc.nextDouble();
+        System.out.println("Enter the Z");
+        double z = sc.nextDouble();
+
+        double result = 0;
+        //3/ x^2 + y^2 - /z/
+        //A+B-C -> A -x^2 , B-y^2 , c-/z/ abs(absolute)
+        result = Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z));
+        System.out.println(result);
+
+        // Scanner steam should be closed after we are done.
+        // JVM will do it, Garbage Collector -> Who wil do it for you
+        // It is not a good practice ->
+
+        sc.close();
+
+
+    }
+
+}
---
+++ src/basics_23Dec/Lab069.java
@@ -0,0 +1,29 @@
+package src.basics_23Dec;
+
+import java.util.Scanner;
+
+public class Lab069 {
+    public static void main(String[] args) {
+
+        // Take a user input and print the information
+
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Enter your Name");
+        String name = user_input.next();
+
+        System.out.println("Enter your Age");
+        int age = user_input.nextInt();
+
+        System.out.println("Enter your Salary");
+        double salary = user_input.nextDouble();
+
+        System.out.println("Your details are " + name +" - "+ age +" - " + salary);
+
+        //System.out.println("Your details are " + name);
+        //        System.out.println("Your age is " + age);
+        //        System.out.println("Your Salary is " + salary);
+        user_input.close();
+
+
+    }
+}
