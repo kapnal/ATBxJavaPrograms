82c2cc9ce8d3d29fbf91227ff816b40021b9f79d
kapnal <kapilnalwar1991@gmail.com>
2024-02-01 19:36:24 +0530

    1st feb 2024

---
+++ src/basics_01_10Dec/Pract.java
@@ -0,0 +1,18 @@
+package src.basics_01_10Dec;
+
+public class Pract {
+    public static void main(String[] args) {
+
+        int b = 10;
+
+       System.out.println(--b + b++ + b--);
+
+        System.out.println(b);
+
+
+        // Exp - 9 , b - 9
+        //Exp - 9 , b - 10
+        //Exp - 10, b - 9
+        // Exp - 9+9+10 =29 , b -
+    }
+}
