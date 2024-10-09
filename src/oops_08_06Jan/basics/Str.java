1e27638b29c081e67cb034a9051727bb8798a051
kapnal <kapilnalwar1991@gmail.com>
2024-10-09 22:28:13 +0530

    09th Oct 2024

---
+++ src/oops_08_06Jan/basics/Str.java
@@ -0,0 +1,12 @@
+package src.oops_08_06Jan.basics;
+
+public class Str {
+
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        str1 = str1.concat("Kapil");
+        System.out.println(str1);
+
+    }
+}
