ea8ee8b20a044693fd2b369be5aea588c61aa114
kapnal <kapilnalwar1991@gmail.com>
2024-10-21 14:49:11 +0530

    21st Oct 2024

---
+++ src/interview/Test1.java
@@ -0,0 +1,13 @@
+package src.interview;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 25;
+
+        System.out.println(Math.max(a,b));
+
+    }
+}
